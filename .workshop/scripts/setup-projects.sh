6eec578753c6c3cf0cf69feaa883cc88e9042f03
Santosh <sgurijal@redhat.com>
2022-01-21 15:22:36 -0500

    updating script to just grant permissions

--- .workshop/scripts/setup-projects.sh
+++ .workshop/scripts/setup-projects.sh
@@ -8,15 +8,6 @@ do
         i=$(($i+1))
         echo "user$i"
         
-	oc new-project rehost-jboss-user$i
         oc adm policy add-cluster-role-to-user cluster-admin user$i 
-
-	oc new-project websphere-pipeline-user$i
-
-
-	oc new-project sample-domain-user$i
-	oc label ns sample-domain-user$i weblogic-operator=enabled
-
-	oc new-project rehost-websphere-user$i
 
 done
